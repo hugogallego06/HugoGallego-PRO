feb6946a5d8ad62e8d1e1a1946852b50d07ac26f
Hugo20052006 <hugo.gallego@cesjuanpablosegundo.es>
2024-10-13 17:58:44 +0200

    ejercicios for del 1 al 10 de borja
    ejercicios for del 1 al 10 de borja

--- Tema3/Ejercicios/src/ejerciciosFor.java
+++ Tema3/Ejercicios/src/ejerciciosFor.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import java.util.Random;
 import java.util.Scanner;
 
 public class ejerciciosFor {
@@ -6,11 +6,12 @@ public class ejerciciosFor {
         //ejercicio1();
         //ejercicio3();
         //ejercicio4();
-
-        ejercicio5();
+        //ejercicio5();
         //ejercicio6();
         //ejercicio7();
         //ejercicio8();
+        //ejercicio9();
+        ejercicio10();
     }
     public static Scanner scanner=new Scanner(System.in);
     public static void ejercicio1(){
@@ -58,6 +59,85 @@ public class ejerciciosFor {
         }
     }
     public static void ejercicio5(){
-
+        System.out.println("Introduce un numero de 0 al 5, este será tu base: ");
+        int base=scanner.nextInt();
+        System.out.println("Introduce un numero de 0 al 5, este será tu exponente: ");
+        int exponente=scanner.nextInt();
+        int total=1;
+        for (int i = 0; i <exponente ; i++) {
+            total*=base;
         }
+        System.out.println(base+" ^ "+exponente+" = "+total);
+    }
+    public static void ejercicio6(){
+        //Mostrar todos los números pares comprendidos entre dos dados.
+        int numeroTotal=13;
+        int numero=1;
+        System.out.println("Numero pares entre "+numero+" y "+numeroTotal+" : " );
+        for (int i = numero; i < numeroTotal; i++) {
+            if (i%2 ==0) {
+                System.out.println("Tu numero es "+i +" y es par");
+            }
+        }
+    }
+    public static void ejercicio7(){
+        //Simúlese el lanzamiento de un dado. Se introducirá el número de veces que se lanza el dado (de no ser positivo, se establecerá a 100).
+        System.out.println("¿Cuantas veces quieres lanzar el dado?");
+        int dado=scanner.nextInt();
+        if (dado < 1) {
+            dado=100;
+            System.out.println("El numero introducido no era positivo por lo que tu numero ahora es "+dado);
+        }
+        for (int i = 1; i < dado+1; i++) {
+            System.out.println("Es la "+i+" vez que lanzas el dado y tu num es: "+(int)(Math.random()*6+1));
+        }
+        }
+    public static void ejercicio8() {
+        //Pedir 10 números.
+        int numeros = 0;
+        int acumulador = 0;
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Introduce " + i + " numero:");
+            numeros = scanner.nextInt();
+            acumulador += numeros;
+        }
+        //Mostrar la media de los números positivos, la media de los números negativos y la cantidad de ceros
+        //if (numeros < 0) {
+        //    System.out.println("La media de los numeros negativos introducidos es " + acumulador / 2);
+        //} else if (numeros > 0) {
+        //    System.out.println("La media de los numeros positivos introducidos es " + acumulador / 2);
+
+        //}
+    }
+    public static void ejercicio9() {
+        //Pedir 10 sueldos. Mostrar su suma, media
+        int sueldo=0;
+        int sumatorio=0;
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Tu "+i+" sueldo es: ");
+            sueldo=scanner.nextInt();
+            sumatorio+=sueldo;
+        }
+        System.out.println("La suma total de sueldos es de : "+sumatorio);
+        System.out.println("La media total de sueldos es de : "+sumatorio/2);
+        //cuantos hay mayores de 1000€.
+
+    }
+    public static void ejercicio10() {
+        //Pedir 6 notas escribir la cantidad de alumnos aprobados, condicionados (=4) y suspensos.
+        int notas = 0;
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Tu nota " + i + " es: ");
+            notas = scanner.nextInt();
+            if (notas > 4) {
+                System.out.println("Aprobado");
+            } else if (notas == 4) {
+                System.out.println("Condicionado");
+            } else {
+                System.out.println("Suspenso");
+            }
+        }
+    }
 }
+
+
