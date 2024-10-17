d53f82d2840d1448d8025181424b4805b68ccda2
Victorgonzalez-ces <hugo.gallego@cesjuanpablosegundo.es>
2024-10-17 14:27:16 +0200

    imte

--- Tema3/Ejercicios/src/ejerciciosFor.java
+++ Tema3/Ejercicios/src/ejerciciosFor.java
@@ -10,7 +10,12 @@ public class ejerciciosFor {
         //ejercicio7();
         //ejercicio8();
         //ejercicio9();
-        ejercicio10();
+        //ejercicio10();
+        //ejercicio11();
+        //ejemplosPalabras();
+        //ejercicio13();
+        //ejercicio14();
+        ejercicio15();
     }
     public static Scanner scanner=new Scanner(System.in);
     public static void ejercicio1(){
@@ -63,24 +68,24 @@ public class ejerciciosFor {
         System.out.println("Introduce un numero de 0 al 5, este será tu exponente: ");
         int exponente=scanner.nextInt();
         int total=1;
-        if (base == 00) {
-            System.out.println("Error numero es 00");
+        if (base == 0) {
+            System.out.println("No se calculan potencias en base 0");
         } else if (exponente == 0) {
-            exponente=1;
-            System.out.printf("Tu exponente era 0 por lo que pasa a ser %d\n",exponente);
+            System.out.println("El resultado de la potencia es 1");
         }else {
             for (int i = 0; i <exponente ; i++) {
                 total*=base;
             }
+            System.out.println(base+" ^ "+exponente+" = "+total);
         }
-        System.out.println(base+" ^ "+exponente+" = "+total);
     }
     public static void ejercicio6(){
         //Mostrar todos los números pares comprendidos entre dos dados.
-        int numeroTotal=13;
-        int numero=1;
-        System.out.println("Numero pares entre "+numero+" y "+numeroTotal+" : " );
-        for (int i = numero; i < numeroTotal; i++) {
+        System.out.println("Di desde que numero quieres calcular ");
+        int numeroInicial=scanner.nextInt();
+        System.out.println("Di hasta que numero quieres calcular " );
+        int numeroFinal=scanner.nextInt();
+        for (int i = numeroInicial; i <= numeroFinal; i++) {
             if (i%2 ==0) {
                 System.out.println("Tu numero es "+i +" y es par");
             }
@@ -94,69 +99,167 @@ public class ejerciciosFor {
             dado=100;
             System.out.println("El numero introducido no era positivo por lo que tu numero ahora es "+dado);
         }
-        for (int i = 1; i < dado+1; i++) {
-            System.out.println("Es la "+i+" vez que lanzas el dado y tu num es: "+(int)(Math.random()*6+1));
+        for (int i = 1; i <= dado; i++) {
+            System.out.println("Es la "+i+" vez que lanzas el dado y tu num es: "+(int)((Math.random()*6)+1));
         }
     }
     public static void ejercicio8() {
         //Pedir 10 números.
-        int numeros=0;
-        int acumulador = 0;
-        for (int i = 1; i < 11; i++) {
+        int acumuladorPositivos=0, acumuladorNegativos=0, numeroCeros=0, numerosPositivos=0, numerosNegativos=0;
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Introduce " + i + " numero:");
-            numeros = scanner.nextInt();
-            acumulador += numeros;
+            int numeros = scanner.nextInt();
+            if (numeros < 0) {
+                acumuladorNegativos+=numeros;
+                numerosNegativos++;
+            } else if (numeros>0){
+                acumuladorPositivos+=numeros;
+                numerosPositivos++;
+            }else {
+                numeroCeros++;
+            }
         }
-        //Mostrar la media de los números positivos, la media de los números negativos y
-        if (numeros < 0) {
-            System.out.println("La media de los numeros negativos introducidos es " + acumulador / 2);
-        } else {
-            System.out.println("La media de los numeros positivos introducidos es " + acumulador / 2);
-        }
-        //La cantidad de ceros
-
+        //Mostrar la media de los números positivos, la media de los números negativos y la cantidad de ceros
+        System.out.println("La media de positivos es : "+(float)(acumuladorPositivos/numerosPositivos));
+        System.out.println("La media de negativos es : "+(float)(acumuladorNegativos/numerosNegativos));
+        System.out.println("El numero de ceros es : "+numeroCeros);
 
 
     }
     public static void ejercicio9() {
         //Pedir 10 sueldos. Mostrar su suma, media
-        int sumatorio=0;
-        int sueldo=0;
-        int contadorMayores=0;
-        for (int i = 1; i < 11; i++) {
+        int sumatorio=0, contadorMayores=0;
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Tu "+i+" sueldo es: ");
-            sueldo=scanner.nextInt();
+            int sueldo=scanner.nextInt();
             sumatorio+=sueldo;
             if (sueldo > 1000) {
                 contadorMayores++;
             }
         }
         System.out.println("La suma total de sueldos es de : "+sumatorio);
-        System.out.println("La media total de sueldos es de : "+sumatorio/2);
+        System.out.println("La media total de sueldos es de : "+(float)(sumatorio/10));
         System.out.println("Sueldos mayores de 1000: "+contadorMayores);
         //cuantos hay mayores de 1000€.
 
     }
     public static void ejercicio10() {
         //Pedir 6 notas escribir la cantidad de alumnos aprobados, condicionados (=4) y suspensos.
-        int aprobado = 0;
-        int condicionado = 0;
-        int suspenso = 0;
-        for (int i = 1; i < 11; i++) {
-            System.out.println("Tu nota " + i + " es: ");
+        int aprobado = 0, condicionado = 0,suspenso = 0;
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Introduce una nota :");
             int notas = scanner.nextInt();
-            if (notas > 4) {
-                aprobado++;
-            } else if (notas == 4) {
-                condicionado++;
-            } else {
-                suspenso++;
+            if (notas>= 0 && notas<=10) {
+                if (notas > 4) {
+                    aprobado++;
+                } else if (notas == 4) {
+                    condicionado++;
+                } else {
+                    suspenso++;
+                }
+            }else {
+                System.out.println("Nota invalida");
             }
-
         }
         System.out.println("Aprobados "+aprobado);
         System.out.println("Condicionados "+condicionado);
         System.out.println("Suspensos "+suspenso);
     }
-}
+    public static void ejercicio11() {
+        System.out.println("Lados cuadrado :");
+        int lados=scanner.nextInt();
+        for (int i = 0; i < lados ; i++) {
+            //System.out.println("\t+\t");
+            for (int j = 0; j < lados ; j++) {
+                //Primera fila o ultima
+                if (i ==0 || i == lados - 1){
+                    System.out.print("\t+\t");
+                }
+                //Primera columna o ultima
+                else if (j == 0 || j == lados - 1) {
+                    System.out.println("\t+\t");
+                }else {
+                    System.out.println("\tº\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void ejemplosPalabras() {
+
+        String palabra1 = "Hola que tal";
+        String palabra2 = "Hola que tal";
+        //replace reemplazar espacios entre textos
+        palabra2 = palabra2.replaceAll(" ", "");
+
+        //Son iguales equals
+        //boolean iguales = palabra1.equals(palabra2);
+        //boolean igualesCAP = palabra1.equalsIgnoreCase(palabra2);
+        //Pasar a mayusculas o a minusculas
+        //palabra1 = palabra1.toUpperCase();
+        //System.out.println(palabra1);
+        //palabra2 = palabra2.toLowerCase();
+        //System.out.println(palabra2);
+        //contiene
+        //boolean contiene= palabra1.contains("que");
+        //cuantas letras hay
+        //palabra1.length();
+        //sacar letra de una palabra
+        //palabra1.charAt(0);
+        //sacar todas las letra de una palabra
+        for (int i = 0; i < palabra1.length(); i++) {
+            char letra = palabra2.charAt(i);
+            System.out.println(letra);
+        }
+    }
+    public static void ejercicio13() {
+        //palabra inversa
+        System.out.println("Escribe una palabra: ");
+        String palabra = scanner.next();
+            for (int i =palabra.length()-1; i >=0 ; i--) {
+                System.out.println(palabra.charAt(i));
+            }
+        }
+    public static void ejercicio14() {
+        System.out.println("Dime de que palabra quieres comprobar si es palindromo");
+        // String palabra = scanner.next();
+        String palabra = "Ejecúción";
+        // NO SE TIENEN EN CUENTA MAYUSCULAS NI ACENTOS
+        palabra = palabra.toLowerCase().replaceAll("ó", "o")
+                .replaceAll("á", "a")
+                .replaceAll("í", "i")
+                .replaceAll("ú", "u")
+                .replaceAll("é", "e");
+        String palabraInversa = "";
+
+        for (int i = palabra.length()-1; i >=0 ; i--) {
+            palabraInversa+=palabra.charAt(i);
+        }
+
+        if (palabra.equals(palabraInversa)){
+            System.out.println("Es palindromo");
+        } else {
+            System.out.println("No es palindromo");
+        }
+
+        //EJEMPLOS PALINDROMOS CORTO (USAR)
+        //for (int i = 0; i <palabra.length()/2 ; i++) {
+        //    palabra=palabra.charAt(i); }
+    }
+    public static void ejercicio15() {
+        System.out.println("Introduce una frase");
+        String frase = scanner.nextLine();
+        String Oraciones="";
+        Oraciones=frase
+        int contadorOraciones=0;
+
+        System.out.printf("Tiene %d oraciones",contadorOraciones);
+
+        int totalLetras=frase.length();
+        System.out.println("Total letras: "+totalLetras);
+
+        }
+    }
+
+
 
